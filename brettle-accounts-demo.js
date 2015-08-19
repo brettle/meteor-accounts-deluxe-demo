d2dbda5af8605d2dcd8478dc728aba03079ed331
Dean Brettle <dean@brettle.com>
2015-08-19 12:36:37 -0700

    Move Delete button to nav bar, and support switching UIs.

--- brettle-accounts-demo.js
+++ brettle-accounts-demo.js
@@ -1,20 +1,35 @@
 if (Meteor.isClient) {
-  Template.home.helpers({
-    userObject: function() {
-      return EJSON.stringify(Meteor.user(), {
-        indent: true
-      });
+  Session.setDefault('uiPkg', 'accounts-ui');
+  Template.registerHelper("useUserAccounts", function(argument){
+    return (Session.get('uiPkg') === 'useraccounts');
+  });
+  Template.nav.events({
+    "click .delete-account": function () {
+      Meteor.call('deleteAccount');
+    },
+    'change #ui-selector': function (event) {
+      Session.set('uiPkg', event.target.value);
     }
   });
-  Template.private.events({
-    "click .delete-account": function() {
-      Meteor.call('deleteAccount');
+  Template.nav.helpers({
+    userObject: function () {
+      return "<pre>" + EJSON.stringify(Meteor.user(), {
+        indent: true
+      }) + "</pre>";
+    }
+  });
+  Template.nav.onRendered(function () {
+    if (! this.popoversInited) {
+      this.$('[data-toggle="popover"]').popover({
+        html: true,
+      });
+      this.popoversInited = true;
     }
   });
 }
 
 Meteor.methods({
-  deleteAccount: function() {
+  deleteAccount: function () {
     Meteor.users.remove(this.userId);
   }
 });
